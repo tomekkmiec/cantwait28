b22afcba5dcb7e0a6509ac15d94b71d8d9b25cdb
Tomek Kmiec <t.kmiec171@gmail.com>
2022-06-05 22:04:57 +0200

    Implement itemModel

--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:cantwait28/models/item_models.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 part 'home_state.dart';
@@ -14,9 +15,18 @@ class HomeCubit extends Cubit<HomeState> {
     _streamSubscription = FirebaseFirestore.instance
         .collection('items')
         .orderBy('release_date')
-        .snapshots().listen(
+        .snapshots()
+        .listen(
       (items) {
-        emit(HomeState(items: items));
+        final itemModels = items.docs.map((doc) {
+          return ItemModel(
+            id: doc['id'],
+            title: doc['title'],
+            imageUrl: doc['image_url'],
+            relaseDate: (doc['relase_date'] as Timestamp).toDate(),
+          );
+        }).toList();
+        emit(HomeState(items: itemModels));
       },
     )..onError(
         (error) {
---
+++ lib/models/item_models.dart
@@ -0,0 +1,12 @@
+class ItemModel {
+  ItemModel({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.relaseDate,
+  });
+  final String id;
+  final String title;
+  final String imageUrl;
+  final DateTime relaseDate;
+}
